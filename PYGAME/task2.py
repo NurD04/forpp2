b6a4abaa54062771738f4092c2529ca998611670
ndb04 <nurumkhanov04@gmail.com>
2023-03-26 01:45:02 +0600

    pygame finished

---
+++ PYGAME/task2.py
@@ -0,0 +1,37 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption('Music_player')
+icon = pygame.image.load('Icon/icons.png')
+pygame.display.set_icon(icon)
+player = pygame.image.load('Icon/carous1.png')
+music_sound = [
+    pygame.mixer.Sound('Icon/rain.mp3'),
+    pygame.mixer.Sound('Icon/adai.mp3'),
+    pygame.mixer.Sound('Icon/ik.mp3')
+]
+
+selected_sound_index = 0
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            #музыканы тандауга возможность
+        elif event.type == pygame.KEYDOWN:
+            #музыканы косад
+            if event.key == pygame.K_UP:
+                music_sound[selected_sound_index].play()
+            # музыканы стоп
+            elif event.key == pygame.K_DOWN:
+                music_sound[selected_sound_index].stop()
+            # музыка артка
+            elif event.key == pygame.K_LEFT:
+                selected_sound_index = (selected_sound_index - 1) % len(music_sound)
+            # музыка алдыга
+            elif event.key == pygame.K_RIGHT:
+                selected_sound_index = (selected_sound_index + 1) % len(music_sound)
+
+    screen.blit(player, (0, 0))
+    pygame.display.update()
